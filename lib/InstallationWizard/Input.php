b381bbd332ec1a9e6f810c89199126a44f0d4b4d
manuelalabor <msites@msites.net>
2011-06-17 10:50:44 +0200

    made Input an abstract class

--- lib/InstallationWizard/Input.php
+++ lib/InstallationWizard/Input.php
@@ -2,26 +2,42 @@
 namespace InstallationWizard;
 
 /**
- * 
+ * An abstract base class which holds information about an input-element for
+ * an InstallationWizard_Step. 
  *
  * @author Manuel Alabor
  * @see InstallationWizard
  * @see InstallationWizard_Step
  */
-class Input {
+abstract class Input {
 	
 	private $caption = '';
-	private $type = '';
 	
 	/**
-	 * 
+	 * Default constructior with caption.
 	 *
 	 * @param $caption
-	 * @param $type
 	 */
-	public function __construct($caption, $type) {
+	public function __construct($caption) {
 		$this->caption = $caption;
-		$this->type = $type;
+	}
+	
+	/**
+	 * Returns the caption for this input-element.
+	 *
+	 * @return caption
+	 */
+	public getCaption() {
+		return $this->caption
+	}
+	
+	/**
+	 * Sets the caption for this input-element.
+	 * 
+	 * @param $caption
+	 */
+	protected setCaption($caption) {
+		$this->caption = $caption;
 	}
 	
 }
